168ec0757dc7c587a37deff562422ff24f13976b
Fernando Ordiales <fernando.ordiales23@gmail.com>
2025-07-28 03:31:06 +0800

    add timeline in resume

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,22 @@
+import Timeline from "@/components/Timeline";
+import Titles from "@/components/Titles";
+
 const Resume = () => {
-  return <div className="py-5 px-10">Resume</div>;
+  return (
+    <div className="w-full h-fit py-4 px-8 max-sm:py-0 max-sm:px-0 flex flex-col gap-5">
+      {/* Work Experience */}
+      <div className="w-full h-fit flex flex-col gap-5">
+        <Titles label="Work Experience" />
+
+        <Timeline />
+      </div>
+
+      {/* Educational Background */}
+      {/* <div className="w-full h-fit flex flex-col gap-5">
+        <Titles label="Educational Background" />
+      </div> */}
+    </div>
+  );
 };
 
 export default Resume;
